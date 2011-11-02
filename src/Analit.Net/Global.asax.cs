0ec333a9f6592cff0b42bab6c08aa7725611a7dc
Золотарев Александр <a.zolotarev@analit.net>
2011-11-02 10:05:51 +0400

    ControllerNotFoundException исключил из числа ошибок

--- src/Analit.Net/Global.asax.cs
+++ src/Analit.Net/Global.asax.cs
@@ -32,6 +32,7 @@ namespace Analit.Net
 			LibAssemblies.Add(Assembly.Load("Common.Web.Ui"));
 			Logger.ErrorSubject = "Ошибка в Analit.Net";
 			Logger.SmtpHost = "box.analit.net";
+			Logger.ExcludeExceptionTypes.Add(typeof(ControllerNotFoundException));
 		}
 
 		void Application_Start(object sender, EventArgs e)
@@ -59,62 +60,6 @@ namespace Analit.Net
 			//  Code that runs on application shutdown
 		}
 
-		/*void Application_Error(object sender, EventArgs e)
-		{
-			var exception = Server.GetLastError();
-
-			if (String.IsNullOrEmpty(Request.UrlReferrer.AbsolutePath))
-			{
-				return;
-			}
-
-			if (!Request.Url.ToString().Contains("favicon.ico"))
-			{
-
-				var builder = new StringBuilder();
-				builder.AppendLine("----UrlReferer-------");
-				builder.AppendLine(Request.UrlReferrer != null ? Request.UrlReferrer.ToString() : String.Empty);
-				builder.AppendLine("----Url-------");
-				builder.AppendLine(Request.Url.ToString());
-				builder.AppendLine("--------------");
-				builder.AppendLine("----Params----");
-				foreach (string name in Request.QueryString)
-					builder.AppendLine(String.Format("{0}: {1}", name, Request.QueryString[name]));
-				builder.AppendLine("--------------");
-
-				builder.AppendLine("----Error-----");
-				do
-				{
-					builder.AppendLine("Message:");
-					builder.AppendLine(exception.Message);
-					builder.AppendLine("Stack Trace:");
-					builder.AppendLine(exception.StackTrace);
-					builder.AppendLine("--------------");
-					exception = exception.InnerException;
-				} while (exception != null);
-				builder.AppendLine("--------------");
-
-				builder.AppendLine("----Session---");
-				try
-				{
-					foreach (string key in Session.Keys)
-					{
-						if (Session[key] == null)
-							builder.AppendLine(String.Format("{0} - null", key));
-						else
-							builder.AppendLine(String.Format("{0} - {1}", key, Session[key]));
-					}
-				}
-				catch (Exception ex)
-				{
-				}
-				builder.AppendLine("--------------");
-
-				_log.Error(builder.ToString());
-			}
-
-		}*/
-
 		void Session_Start(object sender, EventArgs e)
 		{
 			// Code that runs when a new session is started
@@ -135,11 +80,6 @@ namespace Analit.Net
 
 		public void Created(IMonoRailContainer container)
 		{ }
-
-		/*public void Initialized(IMonoRailContainer container)
-		{
-			((DefaultViewComponentFactory)container.GetService<IViewComponentFactory>()).Inspect(Assembly.Load("Analit.Net"));
-		}*/
 
 		void Session_End(object sender, EventArgs e)
 		{
