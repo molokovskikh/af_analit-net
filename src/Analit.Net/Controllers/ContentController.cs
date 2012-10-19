410c7142041fd351445c2f8f21b6ef4f4318e62d
Александр Шилин <a.shilin@analit.net>
2012-10-19 18:55:08 +0400

    Новшество #12277. Ссылка на описание формата xml-прайса для внешних пользователей. Добавил outputter-класс для реализации экранирования внутри тега pre

--- src/Analit.Net/Controllers/ContentController.cs
+++ src/Analit.Net/Controllers/ContentController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using Analit.Net.Filters;
 using Analit.Net.Models;
@@ -27,7 +28,68 @@ namespace Analit.Net.Controllers
 
 		public override string MarkUpToHTML(string source)
 		{
-			return TextileFormatter.FormatString(source);
+			var output = new EscapeOutputter();
+			TextileFormatter.FormatString(source, output);
+			return output.Result;
+		}
+	}
+
+	public class EscapeOutputter : IOutputter
+	{
+		private bool _isEscape;
+		private StringBuilder _resultBuilder;
+
+		public string Result
+		{
+			get { return _resultBuilder.ToString(); }
+		}
+
+		public void Begin()
+		{
+			_resultBuilder = new StringBuilder();
+			_isEscape = false;
+		}
+
+		public void Write(string text)
+		{
+			AppendString(text, false);
+		}
+
+		public void WriteLine(string line)
+		{
+			AppendString(line, true);
+		}
+
+		private void AppendString(string source, bool isLine)
+		{
+			if(CheckEscape(source) && _isEscape) {
+				if(isLine)
+					_resultBuilder.AppendLine(source.Replace("<", "&lt;")
+						.Replace(">", "&gt;"));
+				else
+					_resultBuilder.Append(source.Replace("<", "&lt;")
+						.Replace(">", "&gt;"));
+			}
+			else {
+				_resultBuilder.Append(source);
+			}
+		}
+
+		public void End()
+		{
+		}
+
+		private bool CheckEscape(string source)
+		{
+			if(source.ToLower().Contains("<pre>")) {
+				_isEscape = true;
+				return false;
+			}
+			if(source.ToLower().Contains("</pre>")) {
+				_isEscape = false;
+				return false;
+			}
+			return true;
 		}
 	}
 }
